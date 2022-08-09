eac8cd04d1fef18d5b55fe72e6c0f126589c5987
Prabin-Dukpa21 <prabindukpa891869@gmail.com>
2022-08-10 01:46:56 +0530

    notes for interview

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const _ = require('underscore')
-
 const abc = require('../introduction/intro')
 const loggerModule = require('../logger/logger.js')
 const formatterModule = require('../validator/formatter') 
 const helperModule = require('../util/helper')
 const router = express.Router();
+const App = express.Router();
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
@@ -24,6 +24,48 @@ router.get('/test-me', function (req, res) {
 });
 
 
+App.get("/sol1", function (req, res) {
+    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+    let arr= [1,2,3,5,6,7]
+  
+    let total = 0;
+    for (var i in arr) {
+        total += arr[i];
+    }
+  
+    let lastDigit= arr.pop()
+    let consecutiveSum= lastDigit * (lastDigit+1) / 2
+    let missingNumber= consecutiveSum - total
+  
+    res.send(  { data: missingNumber  }  );
+  });
+ 
+//---------------solution 2----------
+
+  // -write an api which gives the missing number in an array of integers starting from anywhere….e.g [33, 34, 35, 37, 38]: 36 is missing
+  App.get("/sol2", function (req, res) {
+    //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+    let arr= [33, 34, 35, 37, 38]
+    let len= arr.length
+  
+    let total = 0;
+    for (var i in arr) {
+        total += arr[i];
+    }
+  
+    let firstDigit= arr[0]
+    let lastDigit= arr.pop()
+    let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+    let missingNumber= consecutiveSum - total
+   
+    res.send(  { data: missingNumber  }  );
+  });
+ 
+
+
+
+
+
 router.get('/test-you', function(req, res){
     res.send('This is the second routes implementation')
 })
@@ -32,4 +74,5 @@ router.get('/give-me-students-data',function(req, res){
 
 })
 module.exports = router;
+module.exports = App;
 // adding this comment for no reason
